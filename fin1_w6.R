c1180cf649247c9608671e8301a6dff104de2a79
s368 <s368s368s@gmail.com>
2017-10-24 14:50:16 +0200

    Quiz: 1 question.

--- fin1_w6.R
+++ fin1_w6.R
@@ -149,7 +149,13 @@ spot_rates<-c(7.67,8.27,8.81,9.31,9.75,10.16,10.52,10.85,11.15,11.42,11.67)
 n<-14
 spot_rates<-c(7.3,	7.62,	8.1,	8.45,	9.2,	9.64,	10.12,	10.45,	10.75,	11.22,	11.55,	11.92,	12.2,	12.32)
 b<-0.01
+b<-0.005
 
+#Quiz: fin 1 week6.
+n<-10
+b<-0.05
+spot_rates<-c(3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.55, 3.6, 3.65, 3.7)
+  
 #elementary prices with Ho-Lee model i.e. rates are variable.
 for(j in 0:n)#j - periods.
 {
@@ -204,7 +210,8 @@ for(j in 0:n)#j - periods.
   message(paste("j=",j))
   if(j != 0)
   {
-    root<-uniroot(f,lower = 0, upper = 1, tol = 1e-20)
+#    root<-uniroot(f,lower = 0, upper = 1, tol = 1e-20)
+    root<-uniroot(f,lower = 0, upper = 1, tol = 1e-8)
     a[j]<-root$root
   }
 
@@ -258,18 +265,18 @@ for(j in 0:n)#j - periods.
     {
       #      short_rates[j-i+1,j+1]<-el_price[]
       #3 variants: top[i=0]-center[0<i<j]-bottom[i=j]
-      if(0<i && i<j)
+      if(0<i && i<j)# (j-i) vs (j-i+1)
       {
         #        el_price[j-i+1,j+1]<-q*el_price[j-i,j]/(1+a[j]+b*(j-i)) + (1-q)*el_price[j-i+1,j]/(1+a[j]+b*(j-i-1))#Ho-Lee
         bel_price[j-i+1,j+1]<-q*bel_price[j-i,j]/(1+a[j]*exp(b*(j-i))) + (1-q)*bel_price[j-i+1,j]/(1+a[j]*exp(b*(j-i-1)))#BDT
       }
       
-      if(i == j)#bottom
+      if(i == j)#bottom: min short_rate.
       {
         bel_price[j+1,j+1]<-(q*bel_price[j,j])/(1+a[j])#Ho-Lee and BDT (the same!).
       }
       
-      if(i == 0)#top
+      if(i == 0)#top: max short_rate.
       {
         #        el_price[1,j+1]<-(q*el_price[1,j])/(1+a[j]+b*(j-1))#Ho-Lee
         bel_price[1,j+1]<-(q*bel_price[1,j])/(1+a[j]*exp(b*(j-1)))#BDT
@@ -285,18 +292,41 @@ for(j in 0:n)#j - periods.
   
 }#j
 
-# f<-function(x)
-# {
-#   r+ ( x+10 + x^5)
-# }
-# 
-# bf<-body(f)
-# 
-# for(i in 1:2)
-# {
-# 
-#   sol<-uniroot(fun, lower = -100, upper = 100)
-#   message(paste(sol))
-# }
-# 
-# substitute(expression(a + b), list(a = bf[[2]]))
+#excel
+n<-9 # final payment at t=10
+c<-0.1165
+N<-2 # start of swaption
+
+#
+n<-9 # final payment at t=10
+c<-0.039
+N<-3 # start of swaption
+
+#swaption
+for(j in n:0)#j - periods: backwards.
+{
+  for(i in 0:j)#i - set at the same period.
+  {
+    if(j == n)
+    {
+      swaption_price[i+1,j+1]<-(short_rates[i+1,j+1]-c)/(1+short_rates[i+1,j+1])
+    }
+    else
+    {
+      if(j>=N)
+      {
+        swaption_price[i+1,j+1]<-(short_rates[i+1,j+1]-c)/(1+short_rates[i+1,j+1]) + (q*swaption_price[i+1,j+2] + (1-q)*swaption_price[i+2,j+2])/(1+short_rates[i+1,j+1])
+      }
+      
+      if(j<N)
+      {
+        swaption_price[i+1,j+1]<-(q*swaption_price[i+1,j+2] + (1-q)*swaption_price[i+2,j+2])/(1+short_rates[i+1,j+1])
+      }
+      
+      if(j == N)
+      {
+        swaption_price[i+1,j+1]<-max(swaption_price[i+1,j+1],0)
+      }
+    }
+  }
+}
