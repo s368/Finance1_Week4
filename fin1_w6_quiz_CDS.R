bfe4765a829d9dae37e76b990631afe47c97033c
s368 <s368s368s@gmail.com>
2017-10-30 12:20:00 +0100

    Question 5.

--- fin1_w6_quiz_CDS.R
+++ fin1_w6_quiz_CDS.R
@@ -53,3 +53,57 @@ f<-function(x) {d1*(5*(1-x)+10*x)+d2*(105*(1-x)^2+10*(1-x)*x)-100.923497917906}
 rrr<-uniroot(f,lower = 0, upper = 1)
 round(rrr$root,4)*100 # the answer = 2.12
 
+# question 5 (CDS pricing).
+n<-8
+r<-0.01
+S<-218.89/4
+nominal<-1000000
+R<-0.45
+
+remove(discount)
+remove(hazard_rate)
+remove(survival)
+remove(exp_premium)
+remove(pv_premium)
+remove(default)
+remove(ai)
+remove(pv_ai)
+remove(exp_protection)
+remove(pv_protection)
+
+discount<-vector(length = 21)
+hazard_rate<-vector(length = 21)
+survival<-vector(length = 21)
+exp_premium<-vector(length = 21)
+pv_premium<-vector(length = 21)
+default<-vector(length = 21)
+ai<-vector(length = 21)
+pv_ai<-vector(length = 21)
+exp_protection<-vector(length = 21)
+pv_protection<-vector(length = 21)
+
+for(i in 0:n)
+{
+  if(i == 0)
+  {
+    discount[i+1]<-1.
+    hazard_rate[i+1]<-0.020140573040198/2.
+    survival[i+1]<-100.
+  }
+  else
+  {
+    discount[i+1]<-1/(1+r/4)^i
+    hazard_rate[i+1]<-0.020140573040198/2.
+    survival[i+1]<-survival[i]*(1-hazard_rate[i])
+    
+    exp_premium[i+1]<-S*survival[i+1]/100.
+    pv_premium[i+1]<-exp_premium[i+1]*discount[i+1]*nominal*0.0001
+    
+    default[i+1]<-survival[i]*hazard_rate[i]
+    ai[i+1]<-S/2*default[i+1]/100.
+    pv_ai[i+1]<-ai[i+1]*discount[i+1]*nominal*0.0001
+    
+    exp_protection[i+1]<-(1-R)*default[i+1]/100.
+    pv_protection[i+1]<-exp_protection[i+1]*discount[i+1]*nominal
+  }
+}
