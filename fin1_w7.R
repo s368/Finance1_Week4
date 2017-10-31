32893f8ce0c84f83ab2816c19ae3435cd71eeaf1
s368 <s368s368s@gmail.com>
2017-10-31 11:55:11 +0100

    week 7.

---
+++ fin1_w7.R
@@ -0,0 +1,9 @@
+# question 1: n=30 years, c= 5% yearly, principal M=400000
+
+n<-360 # months i.e. 30 years.
+c<-0.05/12 # i.e. monthly rate.
+M<-400000
+
+B<-c*(1+c)^n*M/((1+c)^n-1)
+round(B,2) # answer on question 1.
+
