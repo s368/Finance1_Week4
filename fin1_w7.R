ef73efef8066374accd5a2b3dff357ef451727a2
s368 <s368s368s@gmail.com>
2017-11-05 17:20:04 +0100

    mod

--- fin1_w7.R
+++ fin1_w7.R
@@ -26,9 +26,14 @@ round(a,2) # answer on question 1.
 n<-12*30 # 30 years -> in months
 r<-0.12/12 # year rate -> in month compounding
 p<-100
-a<-r*(1+r)^n*p/((1+r)^n-1)
-round(a,2) # answer on question 1.
+a_monthly<-r*(1+r)^n*p/((1+r)^n-1)
+round(a_monthly,2) # answer on question 1.
 
+n<-30 # 30 years -> in months
+r<-0.12 # year rate -> in month compounding
+p<-100
+a_yearly<-r*(1+r)^n*p/((1+r)^n-1)
+round(a_yearly,2) # answer on question 1.
 
-
+principal_left<-13.725*1.12-12.41*0.960+2*3.0-(0.960-0.941)*(13.725+50+25*1.12^3)
 
